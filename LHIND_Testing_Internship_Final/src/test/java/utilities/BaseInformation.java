61b31d6a07ce0a05321dfa8970b3168ce9b6c20b
4ndr-34 <93264963+4ndr-34@users.noreply.github.com>
2023-05-02 23:10:46 +0200

    Initial steps

---
+++ LHIND_Testing_Internship_Final/src/test/java/utilities/BaseInformation.java
@@ -0,0 +1,44 @@
+package utilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+public class BaseInformation {
+
+    public static BaseInformation getBaseInformation(){
+       return new BaseInformation();
+    }
+
+    private static WebDriver driver;
+
+    public static WebDriver getDriver(){
+        if(driver == null){
+            String browserType = ConfigurationReader.getProperty("browser");
+            switch (browserType){
+                case "chrome":
+                    System.setProperty("webdriver.chrome.driver","src/test/resources/drivers/chromedriver112.exe");
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--remote-allow-origins=*");
+                    driver = new ChromeDriver(options);
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+                case "firefox":
+                    System.setProperty("","");
+                    driver = new FirefoxDriver();
+                    driver.manage().window().maximize();
+                    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+                    break;
+            }
+        }
+        return driver;
+    }
+
+    public static void quit() {
+       driver.quit();
+    }
+
+
+}
---
+++ LHIND_Testing_Internship_Final/src/test/java/utilities/WebElementUtils.java
@@ -0,0 +1,132 @@
+package utilities;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.Color;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class WebElementUtils {
+    WebDriver driver = BaseInformation.getDriver();
+
+    private final BaseInformation baseInformation;
+    private final WaitUtils waitUtils;
+
+
+    public WebElementUtils(BaseInformation baseInformation, Duration defaultDuration) {
+        this.baseInformation = baseInformation;
+        this.waitUtils = new WaitUtils(baseInformation, defaultDuration);
+    }
+
+    public void clickWebElement(WebElement webElement) {
+        waitUtils.waitForElementClickable(webElement)
+                .click();
+    }
+
+    public void javaScriptClick(WebElement webElement) {
+        waitUtils.waitForElementClickable(webElement);
+        JavascriptExecutor executor = (JavascriptExecutor)driver;
+        executor.executeScript("arguments[0].click();", webElement);
+    }
+
+    public void moveMouseToElement(WebElement element) {
+        new Actions(driver)
+                .moveToElement(element)
+                .perform();
+        WaitUtils.waitFor(5000);
+    }
+
+    public void scrollToElement(WebElement element) {
+        ((JavascriptExecutor) driver)
+                .executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public void scrollOffset(int x, int y) {
+        ((JavascriptExecutor) driver)
+                .executeScript("window.scrollBy(arguments[0],arguments[1])", x, y);
+    }
+
+    public void scrollToBottomOfPage() {
+        ((JavascriptExecutor) driver)
+                .executeScript("window.scrollTo(0, document.body.scrollHeight);");
+    }
+
+    public String getTextWithoutSubElements(WebElement element) {
+        String fullText = element.getText()
+                .trim();
+        for (WebElement subElement : element.findElements(By.xpath(".//*"))) {
+            fullText = fullText.replace(subElement.getText().trim(), "");
+        }
+        fullText = fullText.replaceAll("\n", "");
+        return fullText;
+    }
+
+
+    public void sendKeysToElementWithWait(WebElement element, String value, long millsWait) {
+        element.sendKeys(value);
+        WaitUtils.waitFor(millsWait);
+    }
+
+
+    public boolean isElementVisibleWithWait(int mills, WebElement element) {
+        try {
+            waitUtils.waitForElementVisibleWithCustomTime(mills, element);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+
+
+
+    //METODA TE REJA
+
+    public void selectFromDropDownElementByVisibleText(WebElement element, String visibleText) {
+        try {
+            waitUtils.waitForElementVisible(element);
+            Select s = new Select(element);
+            s.selectByVisibleText(visibleText);
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void verifyElementIsDisplayed(WebElement element) {
+        try {
+            waitUtils.waitForElementVisible(element);
+            Assert.assertTrue(element.isDisplayed());
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void verifyElementIsNotDisplayed(WebElement element) {
+        try {
+            Assert.assertTrue(!element.isDisplayed());
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void clearField(WebElement element) {
+        Actions actions = new Actions(driver);
+        try {
+            waitUtils.waitForElementClickable(element);
+            actions.moveToElement(element).doubleClick(element).keyDown(Keys.BACK_SPACE).build().perform();
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    public void verifyColorMatches(WebElement element,String colorCode) {
+        try {
+            waitUtils.waitForElementVisible(element);
+            Assert.assertTrue((Color.fromString(element.getCssValue("color")).asHex()).equals(colorCode));
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
